31007c8c0d74ce6ce76451bce72c8c79c12553c9
Shechev <denisych777@gmail.com>
2022-09-14 17:06:28 +0300

    add Example 004 Dif

---
+++ Example004_Dif/Program.cs
@@ -0,0 +1,10 @@
+﻿//1 этап
+Console.WriteLine("Этап 1");
+int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA/numberB);
+//2 этап
+Console.WriteLine("Этап 2");
+double numberC = 12; //dounle позволяет работать с вещественными числами
+double numberD = 5;
+Console.WriteLine(numberC/numberD);
