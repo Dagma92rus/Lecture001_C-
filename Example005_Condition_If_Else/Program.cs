d0bd88dfe572f2ec26eb18d155c7a12779a86771
Shechev <denisych777@gmail.com>
2022-09-14 17:46:14 +0300

    add Example 005 Condition If-Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите имя пользователя на латинице, например Masha");
+string username = Console.ReadLine();
+
+if (username.ToLower == "masha")//добавление .ToLower изменяет все введёные буквы в буквы нижнего регистра. Благодаря чему проще узнать "особого" пользователя.
+    {
+        Console.WriteLine("Ура это же, Маша!");
+    }
+else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
