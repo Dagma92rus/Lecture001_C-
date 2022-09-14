e3f48597c3508069802f38f8be9378d927e0a779
Shechev <denisych777@gmail.com>
2022-09-14 17:30:00 +0300

    edit Example 003 Sum: new Random().Next(a,b)

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -2,4 +2,4 @@ Console.WriteLine("Твоё имя (на латинской раскладке) 
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
-//System.Console.WriteLine($"Hello или привет, {username} !");
+System.Console.WriteLine($"Hello или привет, {username} !");
